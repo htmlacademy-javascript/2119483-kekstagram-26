b633c219100f3b22a4222526f468706537511ea4
Karina <example@yandex.ru>
2022-06-07 13:35:19 +0300

    HomeTask #2

--- js/main.js
+++ js/main.js
@@ -5,10 +5,7 @@ function getRandomIntValue(min, max) {
     if (min < max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    const temporaryValue = min;
-    min = max;
-    max = temporaryValue;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (min - max + 1)) + max;
   }
   return 'Ошибочно задан диапазон значений';
 }
@@ -23,7 +20,7 @@ getRandomIntValue(200.78, 10.23);
 getRandomIntValue(10.23, 200.78);
 
 function checkLenghtValue(str, maxLength) {
-  if (Number(str.length) <= Number(maxLength)) {
+  if (str.length <= maxLength) {
     return true;
   }
   return false;

23e626cc7e6d7b5b99bf2cff71df61395696f5d0
Karina <example@yandex.ru>
2022-06-18 16:30:37 +0300

    updateted HomeTask#5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
 import {createObjects} from './data.js';
-import './downloading.js';
-import './editing.js';
-import './sending.js';
-import './showing.js';
-import './filtering.js';
 
 createObjects();
